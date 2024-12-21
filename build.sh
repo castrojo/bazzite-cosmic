202b7c715b7c3607ce8fd74f2c50bca080ec465a
Jorge O. Castro <jorge.castro@gmail.com>
2024-12-20 19:37:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 set -euox pipefail
 
-dnf config-manager --set-enabled crb
-dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
-
-dnf -y install @multimedia gstreamer1-plugins-{bad-*,good-*,base} lame*
+dnf -y install @cosmic-desktop @cosmic-desktop-apps
+dnf clean
